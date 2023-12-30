3f3a06a3609e16ccbdfdd259efc1024e44cb7d68
MusyafaRT <musyafart@gmail.com>
2023-12-30 22:04:57 +0700

    detik assignment

---
+++ dist/script.dev.js
@@ -0,0 +1,124 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize the carousel
+  var carousel = new bootstrap.Carousel(document.getElementById('recipeCarousel'), {
+    interval: 2000,
+    // Set your desired interval in milliseconds
+    pause: 'hover' // Pause on mouse enter
+
+  }); // Clone items for seamless autoplay
+
+  var items = document.querySelectorAll('.sponsor .carousel .carousel-item');
+  items.forEach(function (el) {
+    var minPerSlide = 10;
+    var next = el.nextElementSibling;
+
+    for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+        // wrap carousel by using first child
+        next = items[0];
+      }
+
+      var cloneChild = next.cloneNode(true);
+      el.appendChild(cloneChild.children[0]);
+      next = next.nextElementSibling;
+    }
+  }); // Pause and play the carousel on mouse enter and leave
+
+  document.getElementById('recipeCarousel').addEventListener('mouseenter', function () {
+    carousel.pause();
+  });
+  document.getElementById('recipeCarousel').addEventListener('mouseleave', function () {
+    carousel.cycle();
+  });
+  var articleNavLinks = document.querySelectorAll(".article .navbar li");
+  var photoCount = 5;
+  var videoDuration = 105;
+  var formatDuration = new Date(0);
+  formatDuration.setSeconds(videoDuration);
+  var timeString = formatDuration.toISOString().substr(11, 8);
+
+  function createIconElement(iconPath) {
+    var iconElement = document.createElement("img");
+    iconElement.src = iconPath;
+    iconElement.alt = "Icon";
+    iconElement.style.width = "20px";
+    document.body.appendChild(iconElement);
+    return iconElement;
+  }
+
+  articleNavLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      var cards = document.querySelectorAll(".article .card");
+      cards.forEach(function (card) {
+        var countElement = card.querySelector(".count-element");
+
+        if (countElement) {
+          card.removeChild(countElement);
+        }
+      });
+
+      if (link.textContent.trim() === "Foto") {
+        cards.forEach(function (card) {
+          var newCountElement = document.createElement("div");
+          newCountElement.className = "count-element";
+          var fotoIcon = createIconElement("./public/img/foto.svg");
+          newCountElement.appendChild(fotoIcon);
+          var countText = document.createElement("span");
+          countText.textContent = "".concat(photoCount, " Photos");
+          newCountElement.appendChild(countText); // Position the count element at the top right of the image
+
+          newCountElement.style.position = "absolute";
+          newCountElement.style.top = "10px";
+          newCountElement.style.right = "10px"; // Adjusted right positioning
+
+          newCountElement.style.backgroundColor = "rgba(33, 36, 39, 0.50)";
+          newCountElement.style.padding = "5px";
+          newCountElement.style.borderRadius = "5px";
+          newCountElement.style.color = "white";
+          card.appendChild(newCountElement);
+        });
+      } else if (link.textContent.trim() === "Video") {
+        cards.forEach(function (card) {
+          var newCountElement = document.createElement("div");
+          newCountElement.className = "count-element";
+          var videoIcon = createIconElement("./public/img/video.svg");
+          newCountElement.appendChild(videoIcon);
+          var countText = document.createElement("span");
+          countText.textContent = "".concat(timeString);
+          newCountElement.appendChild(countText); // Position the count element at the top right of the image
+
+          newCountElement.style.position = "absolute";
+          newCountElement.style.top = "10px";
+          newCountElement.style.right = "10px"; // Adjusted right positioning
+
+          newCountElement.style.backgroundColor = "rgba(33, 36, 39, 0.50)";
+          newCountElement.style.padding = "5px";
+          newCountElement.style.borderRadius = "5px";
+          newCountElement.style.color = "white";
+          card.appendChild(newCountElement);
+        });
+      }
+    });
+  });
+  var menuIcon = document.querySelector('[data-bs-toggle="collapse"]');
+  var navbarNav = document.getElementById('navbarNav');
+  var navbarCollapse = document.querySelector('.navbar-collapse');
+
+  if (navbarCollapse.classList.contains('show')) {
+    navbarNav.classList;
+  }
+
+  menuIcon.addEventListener('click', function () {
+    navbarNav.classList.toggle('show');
+
+    if (menuIcon.src.includes('menu.svg')) {
+      menuIcon.src = './public/img/close.svg';
+    } else {
+      menuIcon.src = './public/img/menu.svg';
+      navbarNav.classList.remove('show');
+    }
+  });
+});
